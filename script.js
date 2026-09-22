5f191af06245b075648131ea4ca82d0438d21a42
lucassamiguel12-stack <lucassamiguel12@gmail.com>
2026-09-21 20:04:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem() {
+  const mensagem = document.getElementById("mensagem");
+
+  mensagem.textContent =
+    "Você é o meu presente favorito, Clarysse. Eu te amo! ❤️";
+
+  mensagem.style.animation = "aparecer 1s ease";
+}
